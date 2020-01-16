3c6d329aefa33282487fe9bda0a07593c925675a
nicoleefleming <32529892+nicoleefleming@users.noreply.github.com>
2020-01-16 10:34:25 -0700

    Add files via upload
    Commit of code files. README.md for code. main.java is the code for Task 4.

---
+++ homework/homework1/5620HW1/README.md
@@ -0,0 +1,4 @@
+# Homework 1 Code
+	The code in this file is for homework 1 task 4. 
+	The code is in Java. 
+	The code should be runnable from the command line. 
---
+++ homework/homework1/5620HW1/src/main.java
@@ -0,0 +1,12 @@
+import java.io.Console;
+import java.util.*;
+
+
+public class main {
+    public static void main(String[] args)
+    {
+        System.out.println("I will use Java as my language of programming in this course.");
+    }
+}
+
+
